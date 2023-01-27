37658427dc05bd3354d2a08566327904663b3bf5
Md. Fazla Rabby <76914303+mfrabby018@users.noreply.github.com>
2023-01-27 10:18:10 +0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+
+$('.testimonials').slick({
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></i></button>',
+    dots:true,
+    infinite:true,
+    loop:true,
+    autoplay: true
+});
